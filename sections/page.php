5dbd6bab1317bbebcaedf2078489accfa3b91011
Matthias Müller <moeli@moeli.com>
2019-09-18 18:45:27 +0200

    Ready for Kirby 3.2.5

--- sections/page.php
+++ sections/page.php
@@ -7,24 +7,13 @@ $options = Section::$types['pages'];
 /** Extend pages section **/
 $options['computed']['data'] = function () {
     $data = [];
-    if ($this->layout === 'list') {
-        $thumb = [
-            'width'  => 100,
-            'height' => 100
-        ];
-    } else {
-        $thumb = [
-            'width'  => 400,
-            'height' => 400
-        ];
-    }
     foreach ($this->pages as $item) {
         $permissions = $item->permissions();
         $blueprint   = $item->blueprint();
-        $image       = $item->panelImage($this->image, $thumb);
+        $image       = $item->panelImage($this->image);
         $data[] = [
             'id'          => $item->id(),
-            'dragText'    => $item->dragText($this->dragTextType),
+            'dragText'    => $item->dragText(),
             'text'        => $item->toString($this->text),
             'info'        => $item->toString($this->info ?? false),
             'parent'      => $item->parentId(),
